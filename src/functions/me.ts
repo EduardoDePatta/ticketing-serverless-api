226a02066ae20615dd4908136efcb6b3111d974f
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 17:59:46 -0300

    chore: src/functions/me.ts

---
+++ src/functions/me.ts
@@ -0,0 +1,20 @@
+import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+import { getDefaultAuthService } from "../shared/auth/authServiceFactory";
+import { getHttpApiTraceId } from "../shared/http/httpApiTraceId";
+import { requireAuth } from "../shared/http/requireAuth";
+import { mapAuthServiceResult } from "../shared/mapAuthServiceResult";
+
+const authService = getDefaultAuthService();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+    const traceId = getHttpApiTraceId({ event });
+
+    const auth = requireAuth({ event, traceId });
+    if (!auth.ok) {
+        return auth.response;
+    }
+
+    const result = await authService.getById({ id: auth.ctx.userId });
+    return mapAuthServiceResult({ result, success: "ok", traceId });
+};
