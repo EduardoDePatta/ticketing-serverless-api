fd1264d9ef1453701aecb49761c70e859a121e1e
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 18:00:13 -0300

    chore: src/validation/auth/index.ts

---
+++ src/validation/auth/index.ts
@@ -0,0 +1,4 @@
+export { loginInputSchema } from "./loginInputSchema";
+export { logoutInputSchema } from "./logoutInputSchema";
+export { refreshInputSchema } from "./refreshInputSchema";
+export { registerInputSchema } from "./registerInputSchema";
