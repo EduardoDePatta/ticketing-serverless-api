938a2a98ab6e26bbc4a31dd4d208dce941822648
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 17:59:19 -0300

    chore: serverless/iam/statements.ts

--- serverless/iam/statements.ts
+++ serverless/iam/statements.ts
@@ -5,6 +5,7 @@ export const iamRoleStatements = [
             "dynamodb:PutItem",
             "dynamodb:GetItem",
             "dynamodb:Scan",
+            "dynamodb:Query",
             "dynamodb:UpdateItem",
             "dynamodb:DeleteItem",
         ],
@@ -12,6 +13,77 @@ export const iamRoleStatements = [
             {
                 "Fn::GetAtt": ["EventsTable", "Arn"],
             },
+            {
+                "Fn::GetAtt": ["OrdersTable", "Arn"],
+            },
+            {
+                "Fn::Join": [
+                    "/",
+                    [
+                        { "Fn::GetAtt": ["OrdersTable", "Arn"] },
+                        "index/*",
+                    ],
+                ],
+            },
+            {
+                "Fn::GetAtt": ["PaymentsTable", "Arn"],
+            },
+            {
+                "Fn::Join": [
+                    "/",
+                    [
+                        { "Fn::GetAtt": ["PaymentsTable", "Arn"] },
+                        "index/*",
+                    ],
+                ],
+            },
+            {
+                "Fn::GetAtt": ["IdempotencyTable", "Arn"],
+            },
+        ],
+    },
+    {
+        Effect: "Allow",
+        Action: [
+            "dynamodb:PutItem",
+            "dynamodb:GetItem",
+            "dynamodb:UpdateItem",
+            "dynamodb:DeleteItem",
+            "dynamodb:Query",
+        ],
+        Resource: [
+            {
+                "Fn::GetAtt": ["UsersTable", "Arn"],
+            },
+            {
+                "Fn::Join": [
+                    "/",
+                    [
+                        { "Fn::GetAtt": ["UsersTable", "Arn"] },
+                        "index/*",
+                    ],
+                ],
+            },
+            {
+                "Fn::GetAtt": ["RefreshTokensTable", "Arn"],
+            },
+            {
+                "Fn::Join": [
+                    "/",
+                    [
+                        { "Fn::GetAtt": ["RefreshTokensTable", "Arn"] },
+                        "index/*",
+                    ],
+                ],
+            },
+        ],
+    },
+    {
+        Effect: "Allow",
+        Action: ["secretsmanager:GetSecretValue"],
+        Resource: [
+            { Ref: "AuthPepperSecret" },
+            { Ref: "AuthJwtSigningKeySecret" },
         ],
     },
 ];
