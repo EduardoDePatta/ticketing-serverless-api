38baa0f92de7e6128e1153cb793b1717abb9a138
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 17:59:49 -0300

    chore: src/functions/refresh.ts

---
+++ src/functions/refresh.ts
@@ -0,0 +1,22 @@
+import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
+
+import { getDefaultAuthService } from "../shared/auth/authServiceFactory";
+import { getHttpApiTraceId } from "../shared/http/httpApiTraceId";
+import { JsonObjectBodyValidation } from "../shared/http/jsonObjectBodyValidation";
+import { mapAuthServiceResult } from "../shared/mapAuthServiceResult";
+
+const authService = getDefaultAuthService();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+    const traceId = getHttpApiTraceId({ event });
+    const body = JsonObjectBodyValidation.parseOrBadRequest({
+        rawBody: event.body,
+        traceId,
+    });
+    if (!body.ok) {
+        return body.response;
+    }
+
+    const result = await authService.refresh({ input: body.value });
+    return mapAuthServiceResult({ result, success: "ok", traceId });
+};
