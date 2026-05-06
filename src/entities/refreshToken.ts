dd1a78970d7b6a7bb571b521cb70102214325530
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 17:59:38 -0300

    chore: src/entities/refreshToken.ts

---
+++ src/entities/refreshToken.ts
@@ -0,0 +1,11 @@
+export interface RefreshToken {
+    id: string;
+    userId: string;
+    familyId: string;
+    tokenHash: string;
+    expiresAt: string;
+    expiresAtEpoch: number;
+    createdAt: string;
+    revokedAt?: string;
+    replacedById?: string;
+}
