8720cb7ac9afec5e73994ea4ca01e3cd773201cb
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 17:59:31 -0300

    chore: serverless/functions/auth.ts

---
+++ serverless/functions/auth.ts
@@ -0,0 +1,66 @@
+export const authAuthorizerFunction = {
+    handler: "src/functions/authorizer.handler",
+};
+
+export const registerFunction = {
+    handler: "src/functions/register.handler",
+    events: [
+        {
+            httpApi: {
+                path: "/auth/register",
+                method: "post",
+            },
+        },
+    ],
+};
+
+export const loginFunction = {
+    handler: "src/functions/login.handler",
+    events: [
+        {
+            httpApi: {
+                path: "/auth/login",
+                method: "post",
+            },
+        },
+    ],
+};
+
+export const refreshFunction = {
+    handler: "src/functions/refresh.handler",
+    events: [
+        {
+            httpApi: {
+                path: "/auth/refresh",
+                method: "post",
+            },
+        },
+    ],
+};
+
+export const logoutFunction = {
+    handler: "src/functions/logout.handler",
+    events: [
+        {
+            httpApi: {
+                path: "/auth/logout",
+                method: "post",
+            },
+        },
+    ],
+};
+
+export const meFunction = {
+    handler: "src/functions/me.handler",
+    events: [
+        {
+            httpApi: {
+                path: "/auth/me",
+                method: "get",
+                authorizer: {
+                    name: "jwtAuthorizer",
+                },
+            },
+        },
+    ],
+};
