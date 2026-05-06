db3e3ffba4fbdd172f875bb9d5fdd8b36a0fc0db
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 18:00:25 -0300

    chore: src/validation/payment/payOrderInputSchema.ts

---
+++ src/validation/payment/payOrderInputSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+import { billingAddressSchema } from "./addressSchema";
+import { cardSchema } from "./cardSchema";
+
+export const payOrderInputSchema = z.strictObject({
+    card: cardSchema,
+    billingAddress: billingAddressSchema,
+});
+
+export type PayOrderInput = z.output<typeof payOrderInputSchema>;
