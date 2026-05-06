4c30263f58e1fc4b9589c2633f38221cba814a8b
Eduardo De Patta <eduardo.depatta@gmail.com>
2026-05-06 17:59:35 -0300

    chore: src/entities/order.ts

---
+++ src/entities/order.ts
@@ -0,0 +1,15 @@
+export type OrderStatus = "PENDING" | "PAID" | "CANCELLED" | "EXPIRED" | "FAILED";
+
+export interface Order {
+    id: string;
+    customerId: string;
+    eventId: string;
+    quantity: number;
+    status: OrderStatus;
+    totalAmountInCents: number;
+    currency: string;
+    expiresAt: string;
+    paymentId?: string;
+    createdAt: string;
+    updatedAt: string;
+}
